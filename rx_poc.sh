209f21d58fa88c54660ff06920c4699e231ffdde
Sadie <an5dy90@gmail.com>
2024-07-29 07:08:38 -0400

    added fc_temp

--- rx_poc.sh
+++ rx_poc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 city=Casablanca
-curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e "-?[[:digit:]]." | head -1
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e "-?[[:digit:]]+" | head -1)
+fc_temp=$(curl -s wttr.in/$city?T | grep -m 2 '°.' | cut -d 'C' -f 2 | grep -Eo -e "-?[[:digit:]]+" | head -1)
