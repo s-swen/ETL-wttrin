e6d5247c167eb445a8624900c31be183df45f186
Sadie <an5dy90@gmail.com>
2024-07-29 08:00:07 -0400

    added h,d,m,y

--- rx_poc.sh
+++ rx_poc.sh
@@ -2,3 +2,7 @@
 city=Casablanca
 obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e "-?[[:digit:]]+" | head -1)
 fc_temp=$(curl -s wttr.in/$city?T | grep -m 2 '°.' | cut -d 'C' -f 2 | grep -Eo -e "-?[[:digit:]]+" | head -1)
+hour=$(TZ='Morocco/Casablanca' date -u +%H)
+day=$(TZ='Morocco/Casablanca' date -u +%d)
+month=$(TZ='Morocco/Casablanca' date +%m)
+year=$(TZ='Morocco/Casablanca' date +%Y)
