d003c95699446a14c06edb77bca55751305bc25d
Sadie <an5dy90@gmail.com>
2024-07-31 06:47:43 -0400

    added absolute block

--- weekly_stats.sh
+++ weekly_stats.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+echo $(tail -7 synthetic_historical_fc_accuracy.tsv | cut -f6) > scratch.txt
+week_fc=($(echo $(cat scratch.txt)))
+
+for i in {0..6}; do
+    echo ${week_fc[$i]}
+done
+
+for i in {0..6}; do
+    if [ ${week_fc[$i]} -lt 0 ]
+    then
+        week_fc[$i]=$(((-1)*${week_fc[$i]}))
+    fi
+    echo ${week_fc[$i]}
+done
