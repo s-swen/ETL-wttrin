23a91789469f35318f05b824754d4fcaf252e7b5
Sadie <an5dy90@gmail.com>
2024-07-31 05:18:45 -0400

    finished fc_accuracy.sh

--- fc_accuracy.sh
+++ fc_accuracy.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+yesterday_fc=$(tail -2 rx_poc.log | head -1 | cut -d " " -f6)
+today_temp=$(tail -1 rx_poc.log | cut -d " " -f4)
+accuracy=$(($yesterday_fc-$today_temp))
+
+if [ -1 -le $accuracy ] && [ $accuracy -le 1 ]
+then
+    accuracy_range=excellent
+elif [ -2 -le $accuracy ] && [ $accuracy -le 2 ]
+then
+    accuracy_range=good
+elif [ -3 -le $accuracy ] && [ $accuracy -le 3 ] 
+then 
+    accuracy_range=fair
+else
+    accuracy_range=poor
+fi
+
+row=$(tail -1 rx_poc.log)
+year=$(echo $row | cut -d " " -f1)
+month=$(echo $row | cut -d " " -f2)
+day=$(echo $row | cut -d " " -f3)
+echo -e "$year\t$month\t$day\t$today_temp\t$yesterday_fc\t$accuracy\t$accuracy_range" >> historical_fc_accuracy.tsv
