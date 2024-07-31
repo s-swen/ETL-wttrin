5343e4c4be87a84e38c4937cad96d8b266d0d4ea
Sadie <an5dy90@gmail.com>
2024-07-31 06:58:14 -0400

    completed

--- weekly_stats.sh
+++ weekly_stats.sh
@@ -9,7 +9,23 @@ done
 for i in {0..6}; do
     if [ ${week_fc[$i]} -lt 0 ]
     then
-        week_fc[$i]=$(((-1)*${week_fc[$i]}))
+        week_fc[$i]=$(((-1)*week_fc[$i]))
     fi
     echo ${week_fc[$i]}
 done
+
+min=${week_fc[0]}
+max=${week_fc[0]}
+for item in ${week_fc[@]}; do
+    if [[ $min > $item ]]
+    then 
+        min=$item
+    fi
+    if [[ $max < $item ]]
+    then
+        max=$item
+    fi
+done
+
+echo "Min=${min}"
+echo "max=${max}"
